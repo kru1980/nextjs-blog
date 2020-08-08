da1262d879f7af94087968569fd7fdff3d51dd49
kru1980 <kru1980@bk.ru>
2020-08-08 10:28:38 +0500

    reinitialized files

---
+++ pages/secret.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Secret = () => {
+  return <Layout secret>secret page bg orange</Layout>;
+};
+
+export default Secret;
